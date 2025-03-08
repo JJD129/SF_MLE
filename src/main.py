b5b5499ee4f0313af344a11596cab8ecb2a0a4cd
JD <zhenjhen192@gmail.com>
2025-03-07 20:06:12 -0600

    rest of pred endpoint

--- src/main.py
+++ src/main.py
@@ -13,3 +13,22 @@ class InputData(BaseModel):
 
 # pred endpoint
 @app.get("/predict")
+async def predict(input_data: List[InputData]):
+    # convert input JSON to a dataframe
+    input_dicts = [item.features for item in input_data]
+    input_df = preprocessing_input(input_dicts)
+
+    # run model pred
+    probabilities, predictions = predict_model(input_df)
+
+    # format output as JSON
+    results = [
+        {
+            "business_outcome": int(pred),
+            "prediction": int(prob),
+            "feature_inputs": data
+
+        }
+        for data, pred, prob in zip(input_dicts, predictions, probabilities)
+    ]
+    return results
