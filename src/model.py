6b65aa2d5a6f093a44543f89bff5fb7929fcb8c1
JD <zhenjhen192@gmail.com>
2025-03-07 19:37:11 -0600

    model prediction

--- src/model.py
+++ src/model.py
@@ -13,3 +13,19 @@ model_path = os.path.join(artifactsdir, "model.pkl")
 
 # load preprocessing artifacts
 model = joblib.load(model_path)
+
+def predict_model(input_df):
+    """
+    runs trained model on preprocessed input data
+
+    Args: 
+        input_df (pd.DataFrame): preprocessed input features
+
+    Returns:
+        tuple: (probablitlies, predictions)
+    """
+
+    probabilities = model.predict_proba(input_df)
+    predictions = (probabilities >= 0.75).astype(int)
+
+    return(probabilities, predictions)   
