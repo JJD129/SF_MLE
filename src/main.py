1263809f1d3714ee94f88a7b467af65a647966f6
JD <zhenjhen192@gmail.com>
2025-03-09 21:53:45 -0500

    changing feature inputs to output only selected features

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from src.preprocessing import preprocessing_input
 from src.model import predict_model
 from typing import Union
 
+import joblib
+import os
+
+# Load selected features from the artifacts directory
+artifacts_dir = "artifacts"
+selected_features_path = os.path.join(artifacts_dir, "selected_features.pkl")
+selected_features = joblib.load(selected_features_path)  # Load selected features list
 
 # Initialize FastAPI
 app = FastAPI()
@@ -21,7 +28,7 @@ async def predict(input_data: List[Dict[str, Union[str, float, int, None]]]):
         {
             "business_outcome": int(pred[0]),
             "prediction": float(prob[0]),
-            "feature_inputs": data
+            "feature_inputs": {key: data[key] for key in selected_features if key in data}
         }
         for data, pred, prob in zip(input_data, predictions, probabilities)
     ]
