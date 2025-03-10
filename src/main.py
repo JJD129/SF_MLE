d75ad28aaf100d2baee83819cab3b62df7c5e648
JD <zhenjhen192@gmail.com>
2025-03-09 23:22:43 -0500

    logging and custom exception

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ from typing import List, Dict
 from src.preprocessing import preprocessing_input
 from src.model import predict_model
 from typing import Union
-
+from src.logger import logging
+from src.exception import CustomException
 import joblib
 import os
+import sys
 
 # Load selected features from the artifacts directory
 artifacts_dir = "artifacts"
@@ -18,18 +20,23 @@ app = FastAPI()
 # Prediction endpoint
 @app.post("/predict")
 async def predict(input_data: List[Dict[str, Union[str, float, int, None]]]):
-    input_df = preprocessing_input(input_data)
+    try:
+        logging.info("Received request with input data: %s", input_data)
+        input_df = preprocessing_input(input_data)
 
-    # Run model prediction
-    probabilities, predictions = predict_model(input_df)
+        # Run model prediction
+        probabilities, predictions = predict_model(input_df)
 
-    # Format output as JSON
-    results = [
-        {
-            "business_outcome": int(pred),
-            "prediction": float(prob),
-            "feature_inputs": {key: data[key] for key in selected_features if key in data}
-        }
-        for data, pred, prob in zip(input_data, predictions, probabilities)
-    ]
-    return results
+        # Format output as JSON
+        results = [
+            {
+                "business_outcome": int(pred),
+                "prediction": float(prob),
+                "feature_inputs": {key: data[key] for key in selected_features if key in data}
+            }
+            for data, pred, prob in zip(input_data, predictions, probabilities)
+        ]
+        return results
+    except Exception as e:
+        logging.error(f"Error in API request: {str(e)}", exc_info=True)
+        raise CustomException(e, sys)
